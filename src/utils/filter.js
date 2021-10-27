38a3a4a755392dcf86307dbc229b53db8dafbd4b
Olga <olga19972010@yandex.by>
2021-10-27 18:29:49 +0300

    8.2 Refactoring stats logic.

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -10,25 +10,37 @@ const Filter = {
 
 const FilterTimePeriod = {
   [TimePeriod.ALL_TIME]: (films) => films.filter((film) => film.userDetails.alreadyWatched),
-  [TimePeriod.TODAY]: (films) => films.filter((film) => {
-    const today = dayjs();
-    return today.diff(dayjs(film.userDetails.watchingDate), `day`) === 0;
-  }),
-  [TimePeriod.WEEK]: (films) => films.filter((film) => {
-    const today = dayjs();
-    const daysInAWeek = 7;
-    return today.diff(dayjs(film.userDetails.watchingDate), `day`) <= daysInAWeek;
-  }),
-  [TimePeriod.MONTH]: (films) => films.filter((film) => {
-    const today = dayjs();
-    const daysInAMonth = 30;
-    return today.diff(dayjs(film.userDetails.watchingDate), `day`) <= daysInAMonth;
-  }),
-  [TimePeriod.YEAR]: (films) => films.filter((film) => {
-    const today = dayjs();
-    const daysInAYear = 365;
-    return today.diff(dayjs(film.userDetails.watchingDate), `year`) <= daysInAYear;
-  }),
+  [TimePeriod.TODAY]: (films) => {
+    const alreadyWatchedFilms = films.filter((film) => film.userDetails.alreadyWatched);
+    return alreadyWatchedFilms.filter((film) => {
+      const today = dayjs();
+      return today.diff(dayjs(film.userDetails.watchingDate), `day`) === 0;
+    });
+  },
+  [TimePeriod.WEEK]: (films) => {
+    const alreadyWatchedFilms = films.filter((film) => film.userDetails.alreadyWatched);
+    return alreadyWatchedFilms.filter((film) => {
+      const today = dayjs();
+      const daysInAWeek = 7;
+      return today.diff(dayjs(film.userDetails.watchingDate), `day`) <= daysInAWeek;
+    });
+  },
+  [TimePeriod.MONTH]: (films) => {
+    const alreadyWatchedFilms = films.filter((film) => film.userDetails.alreadyWatched);
+    return alreadyWatchedFilms.filter((film) => {
+      const today = dayjs();
+      const daysInAMonth = 30;
+      return today.diff(dayjs(film.userDetails.watchingDate), `day`) <= daysInAMonth;
+    });
+  },
+  [TimePeriod.YEAR]: (films) => {
+    const alreadyWatchedFilms = films.filter((film) => film.userDetails.alreadyWatched);
+    return alreadyWatchedFilms.filter((film) => {
+      const today = dayjs();
+      const daysInAYear = 365;
+      return today.diff(dayjs(film.userDetails.watchingDate), `year`) <= daysInAYear;
+    });
+  },
 };
 
 export {Filter, FilterTimePeriod};
